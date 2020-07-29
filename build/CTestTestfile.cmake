2eb1ed2af6695929a4d974e769a46b67efcc477c
maximeaeva <m.bellot01@gmail.com>
2020-07-29 10:20:55 +0200

    New fresh start trying Makefile

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/maxime.bellot/Documents/WebScrap
-# Build directory: C:/Users/maxime.bellot/Documents/WebScrap/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
